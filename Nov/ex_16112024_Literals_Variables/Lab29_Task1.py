66e4733af72b6e24fe94d95a10268d9fc432970f
Prakash Raj <pythonautomation007@gmail.com>
2024-11-16 18:15:22 +0530

    Completed Python task 1 and 2.

---
+++ Nov/ex_16112024_Literals_Variables/Lab29_Task1.py
@@ -0,0 +1,15 @@
+# Task 1 for the Today
+# Take a 2 input from the user
+# Print the Quotient and Remainder
+# 15 ->  num1
+# 2 -> num2
+# Q -> 7
+# R -> 1
+
+num1 = int(input("Add 1st value"))
+num2 = int(input("Add 2nd value"))
+
+quotient = num1 // num2
+remainder = num1 % num2
+print("Quotient value is:", quotient)
+print("Remainder value is:", remainder)
---
+++ Nov/ex_16112024_Literals_Variables/Lab30_Task2.py
@@ -0,0 +1,18 @@
+# Task for the Today
+# Take a 3 input from the user
+# perform the Addition, sub, mul and div
+
+num1=int(input("Enter the 1st number:"))
+num2=int(input("Enter the 2nd number:"))
+num3=int(input("Enter the 3rd number:"))
+
+add=num1+num2+num3
+sub=num1-num2-num3
+div=num1/num2/num3
+mul=num1*num2*num3
+
+print("Addition is:",add)
+print("Subtraction is:",sub)
+print("Division is:",div)
+print("Multiplication is:",mul)
+
